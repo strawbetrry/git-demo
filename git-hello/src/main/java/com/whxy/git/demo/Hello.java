188dfdc353b797c6e9083d956cd759df024544f1
tt <tt@qq.com>
2023-03-21 19:35:20 +0800

    second commit!

--- git-hello/src/main/java/com/whxy/git/demo/Hello.java
+++ git-hello/src/main/java/com/whxy/git/demo/Hello.java
@@ -3,5 +3,6 @@ package com.whxy.git.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git222!!");
     }
 }
