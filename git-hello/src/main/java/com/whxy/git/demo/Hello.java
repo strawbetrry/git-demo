e1fab68f9b4f5afa365969e4c6904a31227c9028
tt <tt@qq.com>
2023-03-21 19:46:23 +0800

    hot-fix first commit!

--- git-hello/src/main/java/com/whxy/git/demo/Hello.java
+++ git-hello/src/main/java/com/whxy/git/demo/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("hello git!!");
         System.out.println("hello git222!!");
         System.out.println("hello git333!!");
+        System.out.println("hello git444!!");
     }
 }
